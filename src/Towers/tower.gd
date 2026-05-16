d45e8b6470cb88f04f45c36d20b9e571c36c9715
Aminata Tengbe <a.l.tengbe@proton.me>
2026-05-16 19:15:21 +0100

    feat: add upgrade method to tower and connect to notification manager:

--- src/Towers/tower.gd
+++ src/Towers/tower.gd
@@ -10,22 +10,26 @@ class_name Tower
 var is_active: bool
 var is_broken: bool
 
-signal tower_break(tower_position: Vector2, repair_handler: Callable)
+
+signal tower_change(tower_position: Vector2, tower_handler: Callable)
 
 func _ready() -> void:
 	# Subcribe to broken tower state
 	state_machine.broken_state.connect(_on_broken_state)
 	
 	# Broadcast to Notification manager
-	tower_break.connect(NotificationManager._on_tower_break)
+	tower_change.connect(NotificationManager._on_tower_change)
 	add_to_group("Towers")
 
 func _process(_delta):
 	is_active = building.is_active
 
-func rank_up():
+func rank_up() -> void:
 	if current_rank + 1 <= max_rank:
 		current_rank += 1
 
+func _on_rank_up_request() -> void:
+	tower_change.emit(global_position, rank_up)
+
 func _on_broken_state(state: State) -> void:
-	tower_break.emit(global_position, state.repair)
+	tower_change.emit(global_position, state.repair)
--- src/UI/NotificationSystem/Nofication/notification.gd
+++ src/UI/NotificationSystem/Nofication/notification.gd
@@ -2,7 +2,5 @@ extends Control
 
 class_name Notification
 
-var end_handler: Callable
-
 func set_pressed_handler(handler: Callable) -> void:
 	$NotificationBody.pressed.connect(handler)
